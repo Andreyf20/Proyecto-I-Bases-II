7f1074422461929ec01cf2504bed01fb05acf3be
Mauricio <mqbravo@gmail.com>
2020-06-15 20:18:53 -0600

    Added middleware to get user from OAuth payload + dotenv dependency

--- prueba/middleware/auth.ts
+++ prueba/middleware/auth.ts
@@ -19,3 +19,25 @@ export async function generateAuthToken(user) {
   //TODO save token to user
   return token;
 }
+
+export async function getUserFromPayload(req,res,next){
+  try {
+    if(!req.user._id)
+      throw new Error("User not found")
+
+    const user = {
+      _id : 2,
+      nombre : "Mauricio"
+    } //TODO Quitar esto, remplazar por .findOne() con el _id del payload 
+    
+    user['_id'] = req.user._id //Ejemplo de como se obtiene el id del usuario
+
+    req.user = user
+
+    next()
+  } 
+  
+  catch (error) {
+    res.status(401).send({error : error.message})
+  }
+}
