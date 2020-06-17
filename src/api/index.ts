4be31a9dec04f297dc483a0aae5077f8ccc79aa1
Andrey Torres Calderón <atorresf20@gmail.com>
2020-06-17 13:46:41 -0600

    Finished routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,84 @@
-import { checkJwt, getUserFromPayload, generateAuthToken } from "../middleware/auth";
+import { env } from "process";
+
+import {generateAuthToken } from "../middleware/auth";
+
 const express = require('express');
 var router = express.Router();
 
-/*const newProduct=await prisma.createProducto({cantidad:15,codigo:17,enabled:1,provincia:2,nombre:"Prueba3",precio:50})
+const mongoose = require("mongoose");
+const Product = require("../models/producto");
+const authenticationEnabled = false;
 
-const allProducts=await prisma.productoes();
-console.log(allProducts)*/
+var dbConfig = {
+  url: env["MONGO_URL"],
+  user: env["MONGO_USER"],
+  pwd: env["MONGO_PWD"],
+};
 
-router.get('/', async ()=> { 
-  //TODO 
-})
+function getConnection() {
+  if (
+    dbConfig.user !== "NULL" &&
+    dbConfig.pwd !== "NULL" &&
+    authenticationEnabled
+  ) {
+    mongoose
+      .connect(dbConfig.url, {
+        useNewUrlParser: true,
+        user: dbConfig.user,
+        pass: dbConfig.pwd,
+      })
+      .then(() => {
+        console.log("successfully connected to the database");
+      })
+      .catch(() => {
+        console.log("error connecting to the database");
+      });
+  } else {
+    mongoose
+      .connect(dbConfig.url, {
+        useNewUrlParser: true,
+      })
+      .then(() => {
+        console.log("successfully connected to the database");
+      })
+      .catch(() => {
+        console.log("error connecting to the database");
+      });
+  }
+}
+
+getConnection();
+
+router.get("/", async (req, res, next) => {
+  res.send("ROOT DIR");
+  next();
+});
+
+router.post("/agregarProducto", async (req, res, next) => {
+  const newProduct = req.body.producto;
+  // TODO: validar con el lint
+  console.log(newProduct);
+  res.send("Product succesfully added");
+  next();
+});
+
+router.get("/Productos", async (req, res, next) => {
+  // TODO: valiadar los tokens??
+  Product.find()
+    .exec()
+    .then((prod) => {
+      // TODO: validar con el lint??
+      console.log(prod);
+      res.status(200).json(prod);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    })
+    .finally(() => {
+      next();
+    });
+});
 
 router.get('/login', async (req, res)=> {
   try {
@@ -20,5 +89,14 @@ router.get('/login', async (req, res)=> {
     res.send(error)
   }   
 })
+
+
+router.get("/Productos/:lat/:lon", async (req, res, next) => {
+  const lat = req.params.lat;
+  const lon = req.params.lon;
+  console.log("" + lat + " : " + lon);
+  res.send("" + lat + " : " + lon);
+  next();
+});
 
 export = router;
