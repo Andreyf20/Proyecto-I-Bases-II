217ee14376ef198ab90a5de1cb4bd5a3114f323a
Andrey Torres Calderón <atorresf20@gmail.com>
2020-06-17 13:46:41 -0600

    Finished routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,39 +1,106 @@
 import { checkJwt, getUserFromPayload } from "../middleware/auth";
-const express = require('express');
+import { env } from "process";
+const express = require("express");
 var router = express.Router();
+const mongoose = require("mongoose");
+const Product = require("../models/producto");
+const authenticationEnabled = false;
 
-/*const newProduct=await prisma.createProducto({cantidad:15,codigo:17,enabled:1,provincia:2,nombre:"Prueba3",precio:50})
+var dbConfig = {
+  url: env["MONGO_URL"],
+  user: env["MONGO_USER"],
+  pwd: env["MONGO_PWD"],
+};
 
-const allProducts=await prisma.productoes();
-console.log(allProducts)*/
+function getConnection() {
+  if (
+    dbConfig.user !== "NULL" &&
+    dbConfig.pwd !== "NULL" &&
+    authenticationEnabled
+  ) {
+    mongoose
+      .connect(dbConfig.url, {
+        useNewUrlParser: true,
+        user: dbConfig.user,
+        pass: dbConfig.pwd,
+      })
+      .then(() => {
+        console.log("successfully connected to the database");
+      })
+      .catch(() => {
+        console.log("error connecting to the database");
+      });
+  } else {
+    mongoose
+      .connect(dbConfig.url, {
+        useNewUrlParser: true,
+      })
+      .then(() => {
+        console.log("successfully connected to the database");
+      })
+      .catch(() => {
+        console.log("error connecting to the database");
+      });
+  }
+}
 
-router.get('/', async ()=> { 
-  //TODO 
-})
+getConnection();
 
-router.get('/login',checkJwt, getUserFromPayload,async (req, res)=> {
+router.get("/", async (req, res, next) => {
+  res.send("ROOT DIR");
+  next();
+});
+
+router.post("/agregarProducto", async (req, res, next) => {
+  const newProduct = req.body.producto;
+  // TODO: validar con el lint
+  console.log(newProduct);
+  res.send("Product succesfully added");
+  next();
+});
+
+router.get("/Productos", async (req, res, next) => {
+  // TODO: valiadar los tokens??
+  Product.find()
+    .exec()
+    .then((prod) => {
+      // TODO: validar con el lint??
+      console.log(prod);
+      res.status(200).json(prod);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    })
+    .finally(() => {
+      next();
+    });
+});
+
+router.get("/Productos/:lat/:lon", async (req, res, next) => {
+  const lat = req.params.lat;
+  const lon = req.params.lon;
+  console.log("" + lat + " : " + lon);
+  res.send("" + lat + " : " + lon);
+  next();
+});
+
+router.get("/login", checkJwt, getUserFromPayload, async (req, res) => {
   var user;
 
   try {
     try {
-      user = req.user // TODO Uncomment await User.findByCredentials(req.body.email, req.body.password)
+      user = req.user; // TODO Uncomment await User.findByCredentials(req.body.email, req.body.password)
     } catch (error) {
-      res.status(401).send({error : 'Unauthorized',
-                            message : error.message
-                          })
+      res.status(401).send({ error: "Unauthorized", message: error.message });
     }
-    
-    const token = await user.newAuthToken()
 
-    res.send({user,token})
-  } 
-  
-  catch (error) {
-    res.status(500).send({error : 'Server error :/',
-                          message : error.message
-                        });
+    const token = await user.newAuthToken();
+
+    res.send({ user, token });
+  } catch (error) {
+    res.status(500).send({ error: "Server error :/", message: error.message });
   }
-
-})
+});
 
 export = router;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,101 +1,97 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-const bycript = require('bcryptjs')
-import {generateAuthToken} from '../middleware/auth'
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bycript = require("bcryptjs");
+import { generateAuthToken } from "../middleware/auth";
 
-const userSchema = new mongoose.Schema( {
-        name : {
-            type : String,
-            required : [true, "User's name is required"]
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User's name is required"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "User's e-mail is required"],
+      trim: true,
+      lowercase: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) throw new Error("Email is invalid");
+      },
+    },
+
+    birthday: {
+      type: String,
+
+      validate(value) {
+        if (value < 0) throw new Error("Age most be positive number");
+      },
+    },
+
+    password: {
+      type: String,
+      required: [true, "Must specify a password"],
+      trim: true,
+      minlength: [7, "Password must be at least length 7"],
+    },
+
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
         },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-        email : {
-            type : String,
-            required : [true, "User's e-mail is required"],
-            trim : true,
-            lowercase : true,
-            unique : true, 
-            validate(value) {
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
 
-                if(!validator.isEmail(value))
-                    throw new Error('Email is invalid')
-            }
-        },
+  if (!user) throw new Error("Incorrect email or password");
 
-        birthday : {
-            type : String,
+  const isMatch = await bycript.compare(password, user.password);
 
-            validate(value){ 
-                if(value < 0)
-                    throw new Error("Age most be positive number")
-            }
-        },
+  if (!isMatch) throw new Error("Incorrect email or password");
 
-        password : {
-            type : String,
-            required : [true, "Must specify a password"],
-            trim : true,
-            minlength : [7, "Password must be at least length 7"],
-        },
+  return user;
+};
 
-        tokens : [{
-            token : {
-                type : String,
-                required : true
-            }
-        }]
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObj = user.toObject();
 
-    }, 
-    {
-     timestamps : true
-    })
+  delete userObj.password;
+  delete userObj.tokens;
 
-userSchema.statics.findByCredentials = async (email, password)=> {
-    const user = await User.findOne({email})
+  return userObj;
+};
 
-    if(!user)
-        throw new Error("Incorrect email or password")
-    
-    const isMatch = await bycript.compare(password, user.password)
+userSchema.methods.newAuthToken = async function () {
+  const user = this;
+  const token = await generateAuthToken(user);
 
-    if(!isMatch)
-        throw new Error("Incorrect email or password")
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
 
-    return user
-}
-
-userSchema.methods.toJSON = function() {
-    const user = this
-    const userObj = user.toObject()
-
-    delete userObj.password
-    delete userObj.tokens
-
-    return userObj
-}
-
-userSchema.methods.newAuthToken = async function(){
-    const user = this
-    const token = await generateAuthToken(user)
-
-    user.tokens = user.tokens.concat({token})
-    await user.save() 
-
-    return token
-}
+  return token;
+};
 
 //Hash user password before saving
-userSchema.pre('save', async function (next) {
-    const user = this
+userSchema.pre("save", async function (next) {
+  const user = this;
 
-    if(user.isModified("password"))
-        user.password = await bycript.hash(user.password, 8)
+  if (user.isModified("password"))
+    user.password = await bycript.hash(user.password, 8);
 
+  next();
+});
 
-    next()
-} )
+const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model("User", userSchema)
-
-
-module.exports = User
+module.exports = User;
